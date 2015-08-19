7a7d360a4e5712c20f61d80fab542c56287635e4
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2015-08-19 17:28:22 +0800

    Revert "Revert "changing maps""
    This reverts commit 09f8401e20e49078a103696c422542433eac64fa.

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -79,7 +79,7 @@ void init()
     startmenuLocation.Y = 21;
 	/////////////////////////////////////////
 
-	int levelno = 1;
+	int levelno = 5;
 	levelcheck(levelno,level);
 	ifstream inData;
 	inData.open(level);
