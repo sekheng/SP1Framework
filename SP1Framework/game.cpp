acaa43ec973b8cbd42f23e4b89588a7ad2877f5d
spironan <C_tecklee@Hotmail.com>
2015-08-19 09:17:03 +0800

    shift level to another file

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -354,77 +354,9 @@ void renderMap()
 	if (s == Start)
 	{
 		// Set up sample colours, and output shadings
-		const WORD colors[] =
-		{
-        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
-        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
-		};
-		
 		int levelno = 1;
-		string level;
-		int change;
-		if (levelno == 1)
-		{
-			level = "levels1.txt";
-		}
-		else if (levelno == 2)
-		{
-			level = "levels2.txt";
-		}
-		else if (levelno == 3)
-		{
-			level = "levels3.txt";
-		}
-		else if (levelno == 4)
-		{
-			level = "levels4.txt";
-		}
-		else if (levelno == 5)
-		{
-			level = "levels5.txt";
-		}
-		ifstream inData;
-		inData.open(level);
-		string Data;
-		COORD Ttle;
-		Ttle.X = 0;
-		Ttle.Y = 1;
-        int row = 1;    // For collision Detection
-        int col = 0;    // For collision Detection
-
-		while (!inData.eof())
-		{
-            col = 0;
-			Ttle.X = 0;
-			getline(inData, Data);
-			for (unsigned int x = 0; x < Data.length(); x++)
-			{
-				change = Data[x];
-				
-                if ( change == 49 ) {   // For the collision detection
-                    g_map[row][col] = 1;
-                }
-                else {
-                    g_map[row][col] = 0;
-                }
-
-				if ( change == 49 ) {
-					convert(change);
-					console.writeToBuffer(Ttle, change, colors[5]);
-					Ttle.X += 1;
-                    ++col;
-					continue;
-				}
-				convert(change);
-				console.writeToBuffer(Ttle, change, colors[0] );
-				Ttle.X += 1;
-                ++col;
-			}
-            ++row;
-			Ttle.Y += 1;
-		}
-		inData.close();
-    }
+		level(levelno);
+	}
 }
 void renderCharacter()
 {
